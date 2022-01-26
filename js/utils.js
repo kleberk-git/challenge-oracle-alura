a294824d6414854ffd4c626986a32207d18578e8
Kleber Belo <73143439+kleberk-git@users.noreply.github.com>
2022-01-26 05:52:31 -0500

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,40 @@
+//Aqui eu defini uma função para apagar a caixa de texto;
+function apagar_AREA1(){
+	document.getElementById("texto-codificar").value='';
+}
+
+//Aqui eu defini uma função para apagar a caixa de texto;
+function apagar_AREA2(){
+	document.getElementById("texto-codificado").value='';
+}
+
+//Aqui eu defini uma função para selecionar e copiar o todo texto dentro da caixa;
+function copiarTexto() {
+        let textoCopiado = document.getElementById("texto-codificado");
+        textoCopiado.select();
+        textoCopiado.setSelectionRange(0, 99999)
+        document.execCommand("copy");
+        alert("Mensagem copiada.");
+    }
+	
+//Aqui eu defini uma função para substituir uma string por outra definida usando o metodo replace();
+function mudar_Letra_CODIFICADA() {
+	const textarea = document.getElementById('texto-codificar');
+	let enviar1 = textarea.value;
+	let enviar2 = enviar1.replace(/i/gi, "imes").replace(/e/gi, "enter").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat").toLowerCase();
+	document.getElementById('texto-codificado').value = enviar2;
+}
+
+//Aqui eu defini uma função para substituir uma string por outra definida usando o metodo replace(), neste caso invertendo a ordem da substituição;
+function mudar_Letra_DESCODIFICADA() {
+	const textarea = document.getElementById('texto-codificado');
+	let enviar1 = textarea.value;
+	let enviar2 = enviar1.replace(/ai/gi, "a").replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ober/gi, "o").replace(/ufat/gi, "u").toLowerCase();
+	document.getElementById('texto-codificar').value = enviar2;
+}
+
+//Aqui eu defini uma função para acionar no id botões as ações definidas nas funções;
+document.getElementById('enviar-cript').addEventListener('click', mudar_Letra_CODIFICADA);
+document.getElementById('enviar-descript').addEventListener('click', mudar_Letra_DESCODIFICADA);
+document.getElementById('enviar-cript').addEventListener('click', apagar_AREA1);
+document.getElementById('enviar-descript').addEventListener('click', apagar_AREA2);
